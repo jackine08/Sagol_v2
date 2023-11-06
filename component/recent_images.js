48ef9488af42128c3d61827dee4b5da4c6edced6
jackine08 <30682208+jackine08@users.noreply.github.com>
2023-11-06 17:49:03 +0900

    get recent images from local storage

--- component/recent_images.js
+++ component/recent_images.js
@@ -1,45 +1,54 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
 import {Text, View, SafeAreaView, FlatList, Image, ScrollView, StyleSheet, TouchableOpacity} from 'react-native';
-import Get_description from '../axios/get_description';
-import DATA from '../ex/DATA_EX';
+import get_picture_data from "./GalleryPicker"
 
-function Item({navigation, title, description}) {
-
+function Item({navigation, path, description, name}) {
     return(
-    <TouchableOpacity onPress={() => navigation.navigate('Page_imageDescription', {path: title, description: description})}>
-    <View
-        style={{
-        backgroundColor: '#f9c2ff',
-        padding: 10,
-        marginVertical: 8,
-        marginHorizontal: 30,
-        }}>
-        <Image source={title} style={styles.image} />
-    </View>
+    <TouchableOpacity onPress={() => navigation.navigate('Page_imageDescription', {path: path, description: description})}>
+        <View
+            style={{
+            backgroundColor: '#f9c2ff',
+            padding: 10,
+            marginVertical: 8,
+            marginHorizontal: 30,
+            }}>
+            <Image source={{ uri: 'file://' + path }} style={styles.image} />
+        </View>
     </TouchableOpacity>
     )
 };
 
-const Recent_images_grid_view = ({navigation}) => {
-    
-    // const DATA = Get_description();
-    
-    return (
-        <SafeAreaView style={{marginTop: 0}}>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-        
-        <View style={{paddingHorizontal: 3}}>
-            <FlatList
-                data={DATA}
-                renderItem={({item}) => <Item title={item.title} description={item.Description} navigation={navigation} />}
-                numColumns={2}
-            />
-        </View>
+async function fetchData() {
+    const temp = await get_picture_data();
+    console.log(temp);
 
-        </ScrollView>
-        </SafeAreaView>
-    );
+    return temp;
+}
+  
+const Recent_images_grid_view = ({ navigation }) => {
+  const [localData, setLocalData] = useState([]); // 상태 변수 추가
+
+  useEffect(() => {
+    fetchData().then((data) => {
+      setLocalData(data); // 데이터를 상태 변수에 설정
+    });
+  }, []); // 빈 배열을 전달하여 한 번만 실행되도록 설정
+
+  return (
+    <SafeAreaView style={{ marginTop: 0 }}>
+      <>
+        <View>
+          <FlatList
+            data={localData} // 상태 변수 사용
+            keyExtractor={(item) => item.name}
+            renderItem={({ item }) => <Item path={item.path} description={item.description} navigation={navigation} name={item.name} />}
+          />
+        </View>
+      </>
+    </SafeAreaView>
+  );
 };
+
 
 const styles = StyleSheet.create({
     image: {
@@ -47,5 +56,5 @@ const styles = StyleSheet.create({
       height: 100,
     },
   });
-  
+
   export default Recent_images_grid_view;
--- screen/page_imageDescription.js
+++ screen/page_imageDescription.js
@@ -1,35 +1,25 @@
 import React from 'react';
-import {
-  Button,
-  SafeAreaView,
-  Text,
-  View,
-  Alert
-} from 'react-native';
-
-
-import { Image } from 'react-native';
-
-
+import { Button, SafeAreaView, Text, View, Alert, Image } from 'react-native';
 
 function Page_imageDescription({ route }) {
-  const {path, description} = route.params;
-  
+  const { path, description } = route.params;
+  console.log("in PAGE " + path);
+
   return (
     <SafeAreaView>
-      <Text>사진 설명</Text>
-      <Image source={path}/>
-      <Text>{description}</Text>
+      <Image
+        source={{ uri: 'file://' + path }}
+        style={{ width: 200, height: 200 }} // 이미지 크기 설정 (선택 사항)
+      />
       <View>
-            <Button
-                title="다시 듣기"
-                onPress={(()=>Alert.alert('재생중'))}
-            >
-            </Button>
-        </View>
+        <Text>사진 설명: {description}</Text>
+        <Button
+          title="다시 듣기"
+          onPress={() => Alert.alert('재생중')}
+        />
+      </View>
     </SafeAreaView>
   );
 }
-
 
 export default Page_imageDescription;
