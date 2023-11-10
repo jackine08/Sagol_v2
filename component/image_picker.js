c027a3b86af2498c8611fbabbbbd2e3d434064f5
jackine08 <30682208+jackine08@users.noreply.github.com>
2023-11-10 19:38:01 +0900

    refine image_upload

--- component/image_picker.js
+++ component/image_picker.js
@@ -1,44 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View, PermissionsAndroid } from 'react-native';
 import axios from 'axios';
 import RNFS from 'react-native-fs';
-import { setItem, getItem } from './storage';
+import { getItem, setItem } from './storage';
 import get_picture_data from './GalleryPicker';
 import ImageResizer from 'react-native-image-resizer';
 
-async function requestStoragePermission() {
-  try {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-      {
-        title: 'Storage Permission',
-        message: 'This app needs access to your storage to upload images.',
-        buttonNeutral: 'Ask Me Later',
-        buttonNegative: 'Cancel',
-        buttonPositive: 'OK',
-      },
-    );
+const ShowPicker = () => {
+  const [existingItemsRatio, setExistingItemsRatio] = useState('');
 
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      console.log('Storage permission granted');
-      startImageUpload(); // 권한이 허용되면 이미지 업로드 함수 실행
-    } else {
-      console.log('Storage permission denied');
+  const requestStoragePermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+        {
+          title: 'Storage Permission',
+          message: 'This app needs access to your storage to upload images.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('Storage permission granted');
+        startImageUpload(); // Grant permission, start image upload
+      } else {
+        console.log('Storage permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
     }
-  } catch (err) {
-    console.warn(err);
-  }
-}
+  };
 
-async function startImageUpload() {
-  try {
-    const files = await get_picture_data();
+  const startImageUpload = async () => {
+    try {
+      const files = await get_picture_data();
 
-    const uploadPromises = files.map(async (file) => {
-      // Check if the value exists in AsyncStorage before uploading
-      const existingDescription = await getItem("file://"+file.path);
+      const uploadPromises = [];
+      let existingItems = 0;
 
-      if (!existingDescription) {
+      for (const file of files) {
+        // Check if the item exists in AsyncStorage
+        const itemExists = await getItem("file://" + file.path);
+        if (itemExists) {
+          existingItems += 1;
+          console.log(`Item for ${file.path} already exists in AsyncStorage.`);
+          continue; // Skip uploading if the item already exists
+        }
+
         // Resize image before uploading
         const resizedImage = await resizeImage(file.path, file.name);
 
@@ -48,7 +58,7 @@ async function startImageUpload() {
         formdata.append('Image', {
           name: file.name,
           type: 'image/jpeg',
-          uri: "file://"+resizedImage.path,
+          uri: "file://" + resizedImage.path,
         });
         formdata.append('Hash', file.name);
         formdata.append('Id', file.name);
@@ -57,46 +67,52 @@ async function startImageUpload() {
           'Content-Type': 'multipart/form-data',
         };
 
-        const bf_res = await axios.post("http://minigpt4.hcailab.uos.ac.kr/getAltText", formdata, {headers});
+        const bf_res = await axios.post("http://minigpt4.hcailab.uos.ac.kr/getAltText", formdata, { headers });
         console.log(bf_res.data);
         console.log(`Received data from AI for ${file.name}`);
-        setItem("file://"+file.path, bf_res.data[0].Description);
+        setItem("file://" + file.path, bf_res.data[0].Description);
 
         // Remove resized image after upload
         RNFS.unlink(resizedImage.path)
           .then(() => console.log('Resized image removed'))
           .catch((err) => console.error('Error removing resized image:', err));
-      } else {
-        console.log(`Description already exists for ${file.name}`);
+
+        uploadPromises.push(bf_res.data);
       }
-    });
 
-    await Promise.all(uploadPromises);
-    console.log('All images uploaded');
-  } catch (error) {
-    console.error('Error uploading images:', error);
-  }
-}
+      // Update the ratio of existing items to the total number of pictures
+      const ratio = `${existingItems}/${files.length}`;
+      setExistingItemsRatio(ratio);
+      console.log(`Existing Items Ratio: ${ratio}`);
 
-async function resizeImage(originalPath, fileName) {
-  const resizedPath = RNFS.TemporaryDirectoryPath + `/${fileName}`;
-  await ImageResizer.createResizedImage(originalPath, 800, 600, 'JPEG', 80)
-    .then(({ uri }) => {
-      RNFS.moveFile(uri, resizedPath)
-        .then(() => console.log('Image resized'))
-        .catch((err) => console.error('Error moving resized image:', err));
-    })
-    .catch((err) => console.error('Error resizing image:', err));
+      await Promise.all(uploadPromises);
+      console.log('All images uploaded');
+    } catch (error) {
+      console.error('Error uploading images:', error);
+    }
+  };
 
-  return { path: resizedPath, name: fileName };
-}
+  const resizeImage = async (originalPath, fileName) => {
+    const resizedPath = RNFS.TemporaryDirectoryPath + `/${fileName}`;
+    await ImageResizer.createResizedImage(originalPath, 800, 600, 'JPEG', 80)
+      .then(({ uri }) => {
+        RNFS.moveFile(uri, resizedPath)
+          .then(() => console.log('Image resized'))
+          .catch((err) => console.error('Error moving resized image:', err));
+      })
+      .catch((err) => console.error('Error resizing image:', err));
 
-const ShowPicker = () => {
+    return { path: resizedPath, name: fileName };
+  };
+
   return (
     <View>
       <TouchableOpacity style={styles.button} onPress={requestStoragePermission}>
-        <Text>사진 일괄 업로드</Text>
+        <Text>Upload Pictures</Text>
       </TouchableOpacity>
+      <View style={styles.countContainer}>
+        <Text>Existing Items Ratio: {existingItemsRatio}</Text>
+      </View>
     </View>
   );
 };
